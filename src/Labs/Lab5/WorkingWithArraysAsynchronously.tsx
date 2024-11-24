730ab0869323130934db3922af5cb29a115614c9
ryschoi <choi.re@northeastern.edu>
2024-11-23 19:06:25 -0500

    A5 11/23 7:06pm

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -1,16 +1,19 @@
+import React, { useState, useEffect } from "react";
+import * as client from "./client";
 import { FaTrash } from "react-icons/fa6";
 import { FaPlusCircle } from "react-icons/fa";
 import { TiDelete } from "react-icons/ti";
 import { FaPencil } from "react-icons/fa6";
-import React, { useState, useEffect } from "react";
-import * as client from "./client";
+
 export default function WorkingWithArraysAsynchronously() {
   const [todos, setTodos] = useState<any[]>([]);
+
   const editTodo = (todo: any) => {
     const updatedTodos = todos.map(
       (t) => t.id === todo.id ? { ...todo, editing: true } : t);
     setTodos(updatedTodos);
   };
+
   const [errorMessage, setErrorMessage] = useState(null);
   const updateTodo = async (todo: any) => {
     try {
@@ -22,22 +25,27 @@ export default function WorkingWithArraysAsynchronously() {
     }
 
   };
+
   const createTodo = async () => {
     const todos = await client.createTodo();
     setTodos(todos);
   };
+
   const postTodo = async () => {
     const newTodo = await client.postTodo({ title: "New Posted Todo", completed: false, });
     setTodos([...todos, newTodo]);
   };
+
   const fetchTodos = async () => {
     const todos = await client.fetchTodos();
     setTodos(todos);
   };
+
   const removeTodo = async (todo: any) => {
     const updatedTodos = await client.removeTodo(todo);
     setTodos(updatedTodos);
   };
+
   const deleteTodo = async (todo: any) => {
     try {
       await client.deleteTodo(todo);
@@ -52,20 +60,24 @@ export default function WorkingWithArraysAsynchronously() {
   useEffect(() => {
     fetchTodos();
   }, []);
+
   return (
     <div id="wd-asynchronous-arrays">
       <h3>Working with Arrays Asynchronously</h3>
       {errorMessage && (<div id="wd-todo-error-message" className="alert alert-danger mb-2 mt-2">{errorMessage}</div>)}
-      <h4>Todos <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3"
-        id="wd-create-todo" />
-        <FaPlusCircle onClick={postTodo} className="text-primary float-end fs-3 me-3" id="wd-post-todo" /></h4>
+
+      <h4>Todos
+        <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3" id="wd-create-todo" />
+        <FaPlusCircle onClick={postTodo} className="text-primary float-end fs-3 me-3" id="wd-post-todo" />
+      </h4>
       <ul className="list-group">
         {todos.map((todo) => (
           <li key={todo.id} className="list-group-item">
             <FaPencil onClick={() => editTodo(todo)} className="text-primary float-end me-2 mt-1" />
             <FaTrash onClick={() => removeTodo(todo)} className="text-danger float-end mt-1" id="wd-remove-todo" />
             <TiDelete onClick={() => deleteTodo(todo)} className="text-danger float-end me-2 fs-3" id="wd-delete-todo" />
-            <input type="checkbox" className="form-check-input me-2" defaultChecked={todo.completed} onChange={(e) => updateTodo({ ...todo, completed: e.target.checked })} />
+            <input type="checkbox" className="form-check-input me-2 float-start" defaultChecked={todo.completed} 
+            onChange={(e) => updateTodo({ ...todo, completed: e.target.checked })} />
             {!todo.editing ? (todo.title) : (
               <input className="form-control w-50 float-start" defaultValue={todo.title}
                 onKeyDown={(e) => {
@@ -78,9 +90,6 @@ export default function WorkingWithArraysAsynchronously() {
                 }
               />
             )}
-            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-              {todo.title}
-            </span>
           </li>
         ))}
       </ul> <hr />
