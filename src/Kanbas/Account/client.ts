6c2cd460a679d6ee025063aaf1171a07aee041ee
ryschoi <choi.re@northeastern.edu>
2024-11-27 14:23:30 -0500

    Updates to client.ts

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -14,7 +14,7 @@ export const signin = async (credentials: any) => {
   return response.data;
 };
 export const signup = async (user: any) => {
-  const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
+  const response = await axios.post(`${USERS_API}/signup`, user);
   return response.data;
 };
 export const updateUser = async (user: any) => {
--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER || "http://localhost:4000";
 const MODULES_API = `${REMOTE_SERVER}/api/modules`;
 export const updateModule = async (module: any) => {
     const { data } = await axios.put(`${MODULES_API}/${module._id}`, module);
