2647a0d7a3d890fc457b263df5405c3b0e28924f
ryschoi <choi.re@northeastern.edu>
2024-12-05 13:55:31 -0500

    Session and quiz details start

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import store from "./Kanbas/store";
 import { Provider } from "react-redux";
+import Session from "./Kanbas/Account/Session";
 export default function App() {
   return (
     <HashRouter>
       <Provider store={store}>
-        <div>
-          <Routes>
-            <Route path="/" element={<Navigate to="Kanbas" />} />
-            <Route path="/Labs/*" element={<Labs />} />
-            <Route path="/Kanbas/*" element={<Kanbas />} />
-          </Routes>
-        </div>
+        <Session>
+          <div>
+            <Routes>
+              <Route path="/" element={<Navigate to="Kanbas" />} />
+              <Route path="/Labs/*" element={<Labs />} />
+              <Route path="/Kanbas/*" element={<Kanbas />} />
+            </Routes>
+          </div>
+        </Session>
       </Provider>
     </HashRouter>
   );
